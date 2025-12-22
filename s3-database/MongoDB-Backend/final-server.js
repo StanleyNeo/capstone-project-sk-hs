73761c8197c650f5ec3683abbec94a42af53512e
Stanley Neo <geskneo@gmail.com>
2025-12-22 22:25:39 +0800

    Use environment variable for server port

--- s3-database/MongoDB-Backend/final-server.js
+++ s3-database/MongoDB-Backend/final-server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // ========== ✅ MIDDLEWARE ==========
 // GLOBAL CORS
 app.use(cors({
